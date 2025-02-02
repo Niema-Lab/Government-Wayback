4d2c27093a142591dd2a8ec7c42702a0f1e23c3b
Niema Moshiri <niemamoshiri@gmail.com>
2025-02-01 22:23:48 -0800

    Added MD

---
+++ scripts/create_md.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+from os.path import isdir, isfile
+from sys import argv
+assert len(argv) == 3, "USAGE: %s <input_url_txt> <output_url_md>"
+assert isfile(argv[1]), "Input file not found: %s" % argv[1]
+assert not (isfile(argv[2]) or isdir(argv[2])), "Output exists: %s" % argv[2]
+f_in = open(argv[1], 'rt'); f_out = open(argv[2], 'wt')
+for l in f_in:
+    url = l.strip()
+    f_out.write('[%s](%s)\n' % (url, url))
+f_in.close(); f_out.close()
