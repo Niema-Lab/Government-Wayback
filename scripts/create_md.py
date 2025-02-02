c47c544df467fdc8474019a7df4c60c10914c8e0
Niema Moshiri <niemamoshiri@gmail.com>
2025-02-01 22:30:25 -0800

    update

--- scripts/create_md.py
+++ scripts/create_md.py
@@ -1,11 +1,16 @@
 #! /usr/bin/env python3
 from os.path import isdir, isfile
 from sys import argv
+def clean(s):
+    s = s.strip()
+    for k, v in [('﹖','?')]:
+        s = s.replace(k,v)
+    return s
 assert len(argv) == 3, "USAGE: %s <input_url_txt> <output_url_md>"
 assert isfile(argv[1]), "Input file not found: %s" % argv[1]
 assert not (isfile(argv[2]) or isdir(argv[2])), "Output exists: %s" % argv[2]
 f_in = open(argv[1], 'rt'); f_out = open(argv[2], 'wt')
 for l in f_in:
-    url = l.strip()
+    url = clean(l)
     f_out.write('* [%s](%s)\n' % (url, url))
 f_in.close(); f_out.close()
