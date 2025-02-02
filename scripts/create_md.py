87a318f276ed3c0eb4708aabf9295ad74bafdbaf
Niema Moshiri <niemamoshiri@gmail.com>
2025-02-01 22:37:50 -0800

    update

--- scripts/create_md.py
+++ scripts/create_md.py
@@ -1,21 +1,37 @@
 #! /usr/bin/env python3
 from os.path import isdir, isfile
 from sys import argv
+
+# bad characters to replace
 REPLACE = [
     ('﹖', '?'),
     ('﹕', ':'),
     ('ꤷ', '/'),
 ]
+
+# characters to skip URLs
+SKIP = ['?']
+
+# clean a string
 def clean(s):
     s = s.strip()
     for k, v in REPLACE:
         s = s.replace(k,v)
     return s
-assert len(argv) == 3, "USAGE: %s <input_url_txt> <output_url_md>"
-assert isfile(argv[1]), "Input file not found: %s" % argv[1]
-assert not (isfile(argv[2]) or isdir(argv[2])), "Output exists: %s" % argv[2]
-f_in = open(argv[1], 'rt'); f_out = open(argv[2], 'wt')
-for l in f_in:
-    url = clean(l)
-    f_out.write('* [%s](%s)\n' % (url, url))
-f_in.close(); f_out.close()
+
+# main program
+if __name__ == "__main__":
+    assert len(argv) == 3, "USAGE: %s <input_url_txt> <output_url_md>"
+    assert isfile(argv[1]), "Input file not found: %s" % argv[1]
+    assert not (isfile(argv[2]) or isdir(argv[2])), "Output exists: %s" % argv[2]
+    f_in = open(argv[1], 'rt'); f_out = open(argv[2], 'wt')
+    for l in f_in:
+        url = clean(l)
+        skip = False
+        for c in SKIP:
+            if c in url:
+                skip = True
+        if skip:
+            continue
+        f_out.write('* [%s](%s)\n' % (url, url))
+    f_in.close(); f_out.close()
